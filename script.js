fce434497a9c63dfae304e28be234e9a718f4afd
MaxTyson27 <72412722+MaxTyson27@users.noreply.github.com>
2022-01-28 19:23:02 +0300

    added homework to lesson02

--- script.js
+++ script.js
@@ -5,4 +5,19 @@ let rollback;
 let fullPrice;
 let adaptive;
 console.log('Hello developer');
-alert("Hello");
+// alert("Hello");
+
+title = "project";
+screens = "Простые, Сложные, Интерактивные";
+screenPrice = 22;
+rollback = 44;
+fullPrice = 120000;
+adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " долларa");
+console.log("Стоимость разработки сайта " + fullPrice + " долларов");
+console.log(fullPrice * (rollback / 100));
